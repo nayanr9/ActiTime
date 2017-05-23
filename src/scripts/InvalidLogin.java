05b41777adaf55b46eadb4e27d0201cb8da45e01
Raj Nayan <nayanr9@gmail.com>
2017-05-23 12:23:26 +0530

    comment added

--- src/scripts/InvalidLogin.java
+++ src/scripts/InvalidLogin.java
@@ -5,12 +5,14 @@ import org.testng.annotations.Test;
 import gemeric.BaseTest;
 import pom.LoginPage;
 
+
+//there is a new change on my local, hoping this will work fine
 public class InvalidLogin extends BaseTest {
 	@Test(priority=2)
 	public void testLoginInvalid() {
 		LoginPage lp = new LoginPage(driver);
-		lp.setUserName(INPUT_PATH, "ValidLoginLogout", 1, 0);
-		lp.setPassword(INPUT_PATH, "ValidLoginLogout", 1, 0);
+		lp.setUserName(INPUT_PATH, "InvalidLogin", 1, 0);
+		lp.setPassword(INPUT_PATH, "InvalidLogin", 1, 0);
 		lp.clickLogin();
 		lp.errVerification();
 	}
